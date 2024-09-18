54b986a64d5ce9746046891258434023c4ed840d
Alessandra Avelino <alessavelino123@gmail.com>
2024-09-18 11:54:56 -0300

    send msg

--- src/app/contact/Contact.jsx
+++ src/app/contact/Contact.jsx
@@ -24,7 +24,7 @@ function Contact() {
               <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Mensagem</label>
               <textarea id="message" rows="6" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
           </div>
-          <button type="submit" class="py-3 px-5 text-sm font-medium text-center bg-blue-900 text-white rounded-lg  ">Send message</button>
+          <button type="submit" class="py-3 px-5 text-sm font-medium text-center bg-blue-900 text-white rounded-lg  ">Enviar mensagem</button>
       </form>
       <p><strong>Matriz:</strong> João Pessoa</p>
       <p><strong>Filial:</strong> Guarabira</p>
